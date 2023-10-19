42cbb1a97e87e2c888dbabc0751840185514fa26
ChakaliMadhu6302769895 <Gauthamroyalz12324@gmail.com>
2023-10-19 11:26:51 +0530

    Initial commit

---
+++ lib/Strings.dart
@@ -0,0 +1,10 @@
+
+
+
+class Strings{
+
+  static String name = "XXXTentacion Biography";
+  static String para_one =  "Jahseh Dwayne Ricardo Onfroy (January 23, 1998 – June 18, 2018), known professionally as XXXTentacion,[c] was an American rapper and singer-songwriter.[14][15] Though a controversial figure due to his widely publicized legal troubles, XXXTentacion gained a cult following among his young fanbase during his short career with his depression- and alienation-themed music. Critics and fans often credit him for his musical versatility, with his music exploring emo, trap, trap metal, nu metal, indie rock, lo-fi, hip hop, R&B, and punk rock. He is considered to be a leading figure in the establishment of the emo rap and SoundCloud rap genres, which garnered mainstream attention during the mid-to-late 2010s";
+  static String para_two= "Born in Plantation, Florida, XXXTentacion spent most of his childhood in Lauderhill. He began writing music after being released from a juvenile detention center and soon started his music career on SoundCloud in 2013, employing styles and techniques that were unconventional in rap music such as distortion and heavy guitar-backed instrumentals, drawing inspiration from third-wave emo and grunge. In 2014, he formed the underground collective Members Only and alongside other members of the collective soon became a popular figure in SoundCloud rap, a trap music scene that takes elements of lo-fi music and harsh 808s.[17] XXXTentacion gained mainstream attention with the single Look at Me. His debut album 17 (2017) is certified triple-platinum in the US and reached number two on the Billboard 200.";
+
+}
